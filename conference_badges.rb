119effff459c9db698a393cefb5a74dbe47d92ec
Melissa Real <melissa.real@hotmail.com>
2018-04-23 19:08:39 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -25,7 +25,7 @@ end
 
 def printer(attendees) 
   all_together = Array.new
-  all_together.push(batch_badge_creator(attendees),assign_rooms(attendees))
+
  
-  puts all_together[0]
+ print batch_badge_creator(attendees), assign_rooms(attendees))
 end
