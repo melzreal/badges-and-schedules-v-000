961d3660036f4b145308db8fd348e9e342fc0b4f
Melissa Real <melissa.real@hotmail.com>
2018-04-23 19:05:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -28,6 +28,6 @@ def printer(attendees)
   all_together.push(batch_badge_creator(attendees),assign_rooms(attendees))
  
   all_together.each do |show|
-    print show
+    print show.chomp
   end
 end
